d7879079a6ddaa285bf590e247ad3e093372a3e9
Beyond-Gao <beyond.gao@qq.com>
2024-03-14 18:34:34 +0800

    update

---
+++ .history/exercises/macros/macros3_20240314152059.rs
@@ -0,0 +1,22 @@
+// macros3.rs
+//
+// Make me compile, without taking the macro out of the module!
+//
+// Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+use macros::my_macro;
+
+mod macros {
+    macro_rules! my_macro {
+        () => {
+            println!("Check out my macro!");
+        };
+    }
+}
+
+fn main() {
+    my_macro!();
+}
---
+++ .history/exercises/macros/macros3_20240314152153.rs
@@ -0,0 +1,24 @@
+// use macros::my_macro;
+
+// macros3.rs
+//
+// Make me compile, without taking the macro out of the module!
+//
+// Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+
+mod macros {
+    macro_rules! my_macro {
+        () => {
+            println!("Check out my macro!");
+        };
+    }
+    pub(crate) use bar;
+}
+
+fn main() {
+    my_macro!();
+}
